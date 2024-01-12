803e45c45e799d451a8dc0219df2033da6959283
死神未満 <34259833+RoyShen12@users.noreply.github.com>
2024-01-12 20:43:11 +0800

    chore: make usePersistentConnections rubust

--- src/hooks/usePresistState.ts
+++ src/hooks/usePresistState.ts
@@ -9,29 +9,31 @@ export const PersistentKey = 'yacd.closedConns';
 function usePersistentConnections<T extends DataWithId[]>(
   defaultValue: T,
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const readValue = () => {
-    try {
-      const item = window.localStorage.getItem(PersistentKey);
-      return item ? (JSON.parse(item) as T) : defaultValue;
-    } catch (error) {
-      console.warn(`Error reading localStorage key “${PersistentKey}”:`, error);
-      return defaultValue;
-    }
-  };
+  const [storedValue, setStoredValue] = React.useState<T>(defaultValue);
 
-  const [storedValue, setStoredValue] = React.useState<T>(readValue);
+  React.useEffect(() => {
+    setStoredValue(() => {
+      try {
+        const item = window.localStorage.getItem(PersistentKey);
+        return item ? (JSON.parse(item) as T) : defaultValue;
+      } catch (error) {
+        console.warn(`Error reading localStorage key “${PersistentKey}”:”`, error);
+        return defaultValue;
+      }
+    });
+  }, [defaultValue]);
 
-  const setValue = (value: T | ((val: T) => T)) => {
-    try {
-      const newValue = value instanceof Function ? value(storedValue) : value;
-      // 保存状态
-      setStoredValue(newValue);
-      // 保存到 localStorage
-      window.localStorage.setItem(PersistentKey, JSON.stringify(newValue));
-    } catch (error) {
-      console.warn(`Error setting localStorage key “${PersistentKey}”:`, error);
-    }
-  };
+  const setValue = React.useCallback((value: T | ((val: T) => T)) => {
+    setStoredValue((prevState) => {
+      const newValue = value instanceof Function ? value(prevState) : value;
+      try {
+        window.localStorage.setItem(PersistentKey, JSON.stringify(newValue));
+      } catch (error) {
+        console.warn(`Error setting localStorage key “${PersistentKey}”:`, error);
+      }
+      return newValue;
+    });
+  }, []);
 
   return [storedValue, setValue];
 }
