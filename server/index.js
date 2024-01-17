0eb83694bdd19d7a59efebe06450c7cb5027a172
死神未満 <34259833+RoyShen12@users.noreply.github.com>
2024-01-17 21:52:37 +0800

    chore: add compiled js file

---
+++ server/index.js
@@ -0,0 +1,126 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var compression_1 = require("compression");
+var express_1 = require("express");
+var helmet_1 = require("helmet");
+var mongodb_1 = require("mongodb");
+var app = (0, express_1.default)()
+    .use(express_1.default.json({ limit: '128mb' }))
+    .use((0, helmet_1.default)())
+    .use((0, compression_1.default)());
+var secret = process.argv[2];
+var mongo = process.argv[3];
+var s = Buffer.from(secret, 'hex').toString('utf-8');
+var client = new mongodb_1.MongoClient("mongodb://".concat(mongo, "/"), {
+    retryWrites: true,
+    serverSelectionTimeoutMS: 1000,
+    connectTimeoutMS: 1000,
+    authSource: 'admin',
+    authMechanism: 'SCRAM-SHA-256',
+});
+client.connect().then(function (mongoClient) {
+    console.log('mongo connect success');
+    // mongoClient.on('connectionPoolCleared', () => {
+    //   mongoClient.connect()
+    // })
+    console.log('mongoClient === client', mongoClient === client);
+}, function (err) {
+    console.log('mongo connect failed');
+    console.log(err);
+});
+var PersistentKey = 'yacd.closedConns';
+app.use(function (req, res, next) {
+    if (req.headers['x-yacd-auth'] !== s)
+        return res.status(401).json({});
+    next();
+});
+app.get('/get/:key', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, data, length;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                if (req.params.key !== PersistentKey)
+                    return [2 /*return*/, res.status(404).json({})];
+                return [4 /*yield*/, Promise.all([
+                        client.db('openclash').collection('connections').find({}).sort('id').limit(1000).toArray(),
+                        client.db('openclash').collection('connections').countDocuments({}),
+                    ])];
+            case 1:
+                _a = _b.sent(), data = _a[0], length = _a[1];
+                return [2 /*return*/, res.json(data ? { data: data, length: length } : null)];
+        }
+    });
+}); });
+app.post('/set/:key', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var collection, ret;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (req.params.key !== PersistentKey)
+                    return [2 /*return*/, res.status(404).json({})];
+                collection = client.db('openclash').collection('connections');
+                return [4 /*yield*/, Promise.all(req.body.map(function (d) {
+                        return collection.updateOne({ id: d.id }, { $setOnInsert: d }, { upsert: true });
+                    }))];
+            case 1:
+                ret = _a.sent();
+                // const ret = await collection.bulkWrite(
+                //   req.body.map(d => ({
+                //     updateOne: {
+                //       filter: { id: d.id },
+                //       update: { $setOnInsert: d },
+                //       upsert: true,
+                //     },
+                //   }))
+                // )
+                // const newData = new DataModel({ key, data: req.body.data })
+                // await newData.save()
+                // console.log(`Data saved successfully!`)
+                console.log(ret.reduce(function (p, c) {
+                    p.modifiedCount += c.modifiedCount;
+                    p.upsertedCount += c.upsertedCount;
+                    p.matchedCount += c.matchedCount;
+                    return p;
+                }, { modifiedCount: 0, upsertedCount: 0, matchedCount: 0 }));
+                return [2 /*return*/, res.json({ message: "suc" })];
+        }
+    });
+}); });
+var PORT = process.env.PORT || 3001;
+app.listen(PORT, function () { return console.log("Server running on port ".concat(PORT)); });
